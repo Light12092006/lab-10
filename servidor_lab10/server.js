1f0b415d0b53bcd7403a456486272847d7fe29c3
Light12092006 <jgcfabris12@gmail.com>
2025-05-18 15:50:23 -0300

    lab

--- servidor_lab10/server.js
+++ servidor_lab10/server.js
@@ -1,2 +1,26 @@
+const http = require('http');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { render } = require('ejs');
+const MongoClient = require("mongodb").MongoClient;
+
 const uri = `mongodb+srv://jgcfabris12:12092006@joao.5bzjezq.mongodb.net/?retryWrites=true&w=majority&appName=Joao`
 
+const client = new MongoClient(uri, { useNewUrlParser: true});
+var dbo = client.db("bd_completo");
+var usuarios = dbo.collection("usuarios");
+
+var app = express();
+app.use(express.static('./public'));
+app.set('view engine','ejs');
+app.set('views', '/views');
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.listen(80, () =>{
+    console.log("servidor rodando");
+})
+
+app.get('/', (req,res) => {
+    res.redirect("cadastro_carros.html")
+});
